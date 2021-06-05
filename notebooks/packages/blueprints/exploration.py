9fdafe5948d85ceee03f4bab23e592359199a275
Jens Albrecht <jens.albrecht@th-nuernberg.de>
2021-06-05 16:41:30 +0200

    minor changes

--- notebooks/packages/blueprints/exploration.py
+++ notebooks/packages/blueprints/exploration.py
@@ -37,6 +37,35 @@ def idf(df, column='tokens', preprocess=None, min_df=2):
     return idf_df
     
 
+
+from wordcloud import WordCloud
+from collections import Counter
+from matplotlib import pyplot as plt
+
+def wordcloud(word_freq, title=None, max_words=200, stopwords=None):
+
+    wc = WordCloud(width=800, height=400, 
+                   background_color= "black", colormap="Paired", 
+                   max_font_size=150, max_words=max_words)
+    
+    # convert data frame into dict
+    if type(word_freq) == pd.Series:
+        counter = Counter(word_freq.fillna(0).to_dict())
+    else:
+        counter = word_freq
+
+    # filter stop words in frequency counter
+    if stopwords is not None:
+        counter = {token:freq for (token, freq) in counter.items() 
+                              if token not in stopwords}
+    wc.generate_from_frequencies(counter)
+ 
+    plt.title(title) 
+
+    plt.imshow(wc, interpolation='bilinear')
+    plt.axis("off")
+    
+
 import random
 import re
 import textacy
